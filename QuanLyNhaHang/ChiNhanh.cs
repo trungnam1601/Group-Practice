33b8206089ca8e93cebf275804e43bf1fb15d847
phanminh172 <55455657+phanminh172@users.noreply.github.com>
2021-04-29 06:10:15 +0700

    create DTO

---
+++ QuanLyNhaHang/ChiNhanh.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhaHang.DTO
+{
+    public class ChiNhanh
+    {
+        private string maCN;
+        private string tenCN;
+        private string diaChiCN;
+
+        public string MaCN { get => maCN; set => maCN = value; }
+        public string TenCN { get => tenCN; set => tenCN = value; }
+        public string DiaChiCN { get => diaChiCN; set => diaChiCN = value; }
+
+        public ChiNhanh(string maCN, string tenCN,string diaChiCN)
+        {
+            this.MaCN = maCN;
+            this.TenCN = tenCN;
+            this.DiaChiCN = diaChiCN;
+        }
+        public ChiNhanh(DataRow row)
+        {
+            this.MaCN = row["MaCN"].ToString();
+            this.TenCN = row["TenCN"].ToString();
+            this.DiaChiCN = row["DiaChiCN"].ToString();
+        }
+    }
+}
---
+++ QuanLyNhaHang/NhanVien.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhaHang.DTO
+{
+    public class NhanVien
+    {
+        private string maNV;
+        private string tenNV;
+        private string luong;
+        private string ngaySinh;
+        private string gioiTinh;
+        private string diaChi;
+        private string maCN;
+        private string maBP;
+
+        public string MaBP { get => maBP; set => maBP = value; }
+        public string MaCN { get => maCN; set => maCN = value; }
+        public string DiaChi { get => diaChi; set => diaChi = value; }
+        public string GioiTinh { get => gioiTinh; set => gioiTinh = value; }
+        public string NgaySinh { get => ngaySinh; set => ngaySinh = value; }
+        public string Luong { get => luong; set => luong = value; }
+        public string TenNV { get => tenNV; set => tenNV = value; }
+        public string MaNV { get => maNV; set => maNV = value; }
+
+        public NhanVien(string maNV, string tenNV, string luong, string ngaySinh, string gioiTinh, string diaChi, string maCN, string maBP)
+        {
+            this.MaNV = maNV;
+            this.TenNV = tenNV;
+            this.Luong = luong;
+            this.NgaySinh = ngaySinh;
+            this.GioiTinh = gioiTinh;
+            this.DiaChi = diaChi;
+            this.MaCN = maCN;
+            this.MaBP = maBP;
+        }
+        public NhanVien(DataRow row)
+        {
+            this.MaNV = row["MaNV"].ToString();
+            this.TenNV = row["HoTenNV"].ToString();
+            this.Luong = row["Luong"].ToString();
+            this.NgaySinh = row["NgaySinh"].ToString();
+            this.GioiTinh = row["GioiTinh"].ToString();
+            this.DiaChi = row["DiaChi"].ToString();
+            this.MaCN = row["MaCN"].ToString();
+            this.MaBP = row["MaBP"].ToString();
+        }
+    }
+}
