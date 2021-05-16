6b5880a43471c0b713020c3e01fb3804a3110415
phanminh172 <55455657+phanminh172@users.noreply.github.com>
2021-05-16 09:59:13 +0700

    Add files via upload

---
+++ QuanLyNhaHang/frmCapNhatGoiMon.Designer.cs
@@ -0,0 +1,346 @@
+﻿
+namespace QuanLyNhaHang
+{
+    partial class frmCapNhatGoiMon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnThemMonAn = new System.Windows.Forms.Button();
+            this.cmbNhomMA = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtMaPYC = new System.Windows.Forms.TextBox();
+            this.txtSoLuong = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.txtGiamGia = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.cmbChuyenBan = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.cmbMA = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dtgvCapNhatGoiMon = new System.Windows.Forms.DataGridView();
+            this.panel4.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvCapNhatGoiMon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 65);
+            this.panel1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button4.Location = new System.Drawing.Point(146, 189);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 35);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Xóa Món Ăn";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // btnThemMonAn
+            // 
+            this.btnThemMonAn.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnThemMonAn.Location = new System.Drawing.Point(151, 230);
+            this.btnThemMonAn.Name = "btnThemMonAn";
+            this.btnThemMonAn.Size = new System.Drawing.Size(112, 35);
+            this.btnThemMonAn.TabIndex = 16;
+            this.btnThemMonAn.Text = "Thêm Món Ăn";
+            this.btnThemMonAn.UseVisualStyleBackColor = true;
+            this.btnThemMonAn.Click += new System.EventHandler(this.btnThemMonAn_Click);
+            // 
+            // cmbNhomMA
+            // 
+            this.cmbNhomMA.FormattingEnabled = true;
+            this.cmbNhomMA.Location = new System.Drawing.Point(151, 55);
+            this.cmbNhomMA.Name = "cmbNhomMA";
+            this.cmbNhomMA.Size = new System.Drawing.Size(121, 21);
+            this.cmbNhomMA.TabIndex = 1;
+            this.cmbNhomMA.SelectedIndexChanged += new System.EventHandler(this.cmbNhomMA_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(18, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chọn nhóm món ăn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(80, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mã PYC";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label7.Location = new System.Drawing.Point(81, 107);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 19);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Số lượng";
+            // 
+            // txtMaPYC
+            // 
+            this.txtMaPYC.Location = new System.Drawing.Point(151, 29);
+            this.txtMaPYC.Name = "txtMaPYC";
+            this.txtMaPYC.Size = new System.Drawing.Size(121, 20);
+            this.txtMaPYC.TabIndex = 8;
+            // 
+            // txtSoLuong
+            // 
+            this.txtSoLuong.Location = new System.Drawing.Point(151, 109);
+            this.txtSoLuong.Name = "txtSoLuong";
+            this.txtSoLuong.Size = new System.Drawing.Size(121, 20);
+            this.txtSoLuong.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button1.Location = new System.Drawing.Point(676, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Thanh toán";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.textBox2);
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Controls.Add(this.button7);
+            this.panel4.Controls.Add(this.txtGiamGia);
+            this.panel4.Controls.Add(this.button6);
+            this.panel4.Controls.Add(this.cmbChuyenBan);
+            this.panel4.Controls.Add(this.button5);
+            this.panel4.Controls.Add(this.button1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 363);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(800, 87);
+            this.panel4.TabIndex = 16;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(411, 15);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(341, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 19);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Tổng tiền";
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button7.Location = new System.Drawing.Point(553, 6);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(112, 35);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "Cập Nhật";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // txtGiamGia
+            // 
+            this.txtGiamGia.Location = new System.Drawing.Point(192, 48);
+            this.txtGiamGia.Name = "txtGiamGia";
+            this.txtGiamGia.Size = new System.Drawing.Size(123, 20);
+            this.txtGiamGia.TabIndex = 16;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button6.Location = new System.Drawing.Point(192, 6);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(123, 35);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "Giảm giá";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // cmbChuyenBan
+            // 
+            this.cmbChuyenBan.FormattingEnabled = true;
+            this.cmbChuyenBan.Location = new System.Drawing.Point(10, 47);
+            this.cmbChuyenBan.Name = "cmbChuyenBan";
+            this.cmbChuyenBan.Size = new System.Drawing.Size(121, 21);
+            this.cmbChuyenBan.TabIndex = 18;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button5.Location = new System.Drawing.Point(10, 6);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(123, 35);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Chuyển bàn";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.txtSoLuong);
+            this.panel2.Controls.Add(this.cmbMA);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.cmbNhomMA);
+            this.panel2.Controls.Add(this.btnThemMonAn);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.txtMaPYC);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 65);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(328, 298);
+            this.panel2.TabIndex = 17;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button2.Location = new System.Drawing.Point(146, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 35);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Tìm Kiếm";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // cmbMA
+            // 
+            this.cmbMA.FormattingEnabled = true;
+            this.cmbMA.Location = new System.Drawing.Point(151, 82);
+            this.cmbMA.Name = "cmbMA";
+            this.cmbMA.Size = new System.Drawing.Size(121, 21);
+            this.cmbMA.TabIndex = 19;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label8.Location = new System.Drawing.Point(55, 81);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 19);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Chọn món ăn";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.dtgvCapNhatGoiMon);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(334, 65);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(466, 298);
+            this.panel3.TabIndex = 18;
+            // 
+            // dtgvCapNhatGoiMon
+            // 
+            this.dtgvCapNhatGoiMon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvCapNhatGoiMon.Location = new System.Drawing.Point(0, 0);
+            this.dtgvCapNhatGoiMon.Name = "dtgvCapNhatGoiMon";
+            this.dtgvCapNhatGoiMon.Size = new System.Drawing.Size(466, 242);
+            this.dtgvCapNhatGoiMon.TabIndex = 0;
+            // 
+            // frmCapNhatGoiMon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmCapNhatGoiMon";
+            this.Text = "Cập Nhật Gọi Món";
+            this.Load += new System.EventHandler(this.frmCapNhatGoiMon_Load);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvCapNhatGoiMon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cmbNhomMA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtMaPYC;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnThemMonAn;
+        private System.Windows.Forms.TextBox txtSoLuong;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox txtGiamGia;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ComboBox cmbChuyenBan;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox cmbMA;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dtgvCapNhatGoiMon;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ QuanLyNhaHang/frmCapNhatGoiMon.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuanLyNhaHang.Helper;
+namespace QuanLyNhaHang
+{
+    public partial class frmCapNhatGoiMon : Form
+    {
+        List<NhomMA> nhomMA = new List<NhomMA>();
+        List<MonAn> monAn = new List<MonAn>();
+        public frmCapNhatGoiMon()
+        {
+            InitializeComponent();
+        }
+        
+        private void frmCapNhatGoiMon_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = new Helper.Define().GetConnection();
+            conn.Open();
+            SqlCommand cmdNMA = new SqlCommand("SELECT * FROM NhomMonAn", conn);
+            SqlDataReader drNMA = cmdNMA.ExecuteReader();
+            while (drNMA.Read())
+            {
+                cmbNhomMA.Items.Add(drNMA["TenNhomMA"]);
+                nhomMA.Add(new NhomMA()
+                {
+                    MaNhomMA = drNMA["MaNhomMA"] as string,
+                    TenNhomMA = drNMA["TenNhomMA"] as string
+                });
+            }
+            conn.Close();
+
+            conn.Open();
+            SqlCommand cmdMA = new SqlCommand("SELECT * FROM MonAn", conn);
+            SqlDataReader drMA = cmdMA.ExecuteReader();
+            while (drMA.Read())
+            {
+                monAn.Add(new MonAn()
+                {
+                    MaMA=drMA["MaMA"]as string,
+                    TenMA= drMA["TenMA"] as string,
+                    DVT = drMA["DVT"] as string,
+                    GiaBan = ((int)drMA["GiaBan"]),
+                    MaNhomMA = drMA["MaNhomMA"] as string,
+                    TrangThai = drMA["TrangThai"] as string,
+                });
+            }
+            conn.Close();
+            
+        }
+
+        private string[] GetFoodByID(string maNhomMA)
+        {
+            return monAn.Where(line => line.MaNhomMA == maNhomMA).Select(l => l.TenMA).ToArray();
+        }
+        private string[] GetIdByName(string TenMA)
+        {
+            return monAn.Where(line => line.TenMA == TenMA).Select(l => l.MaMA).ToArray();
+        }
+
+        [Serializable]
+        class NhomMA
+        {
+            public string MaNhomMA { get; set; }
+            public string TenNhomMA { get; set; }
+        }
+        [Serializable]
+        class MonAn
+        {
+            public string MaMA { get; set; }
+            public string TenMA { get; set; }
+            public string DVT { get; set; }
+            public int GiaBan { get; set; }
+            public string MaNhomMA { get; set; }
+            public string TrangThai { get; set; }
+        }
+
+        private void cmbNhomMA_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbMA.Items.Clear();
+            string MaNhomMA = nhomMA[cmbNhomMA.SelectedIndex].MaNhomMA;
+            foreach (string tenMA in GetFoodByID(MaNhomMA))
+            {
+                cmbMA.ValueMember = "MaMA";
+                this.cmbMA.Items.Add(tenMA);
+            }
+        }
+
+        DataSet GetRequest()
+        {
+            DataSet data = new DataSet();
+            string query = "select MaMA, DonGiaMA, SoLuongMA, DonGiaMA*SoLuongMA as TongTien from ChiTietYeuCau Where MaPYC = '"+txtMaPYC.Text+"'";
+            using (SqlConnection connection = new SqlConnection(Helper.Define.dataSource))
+            {
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dtgvCapNhatGoiMon.DataSource = GetRequest().Tables[0];
+        }
+
+        private void btnThemMonAn_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(Helper.Define.dataSource);
+            SqlDataAdapter aa = new SqlDataAdapter("PROC_INSERT_YEUCAU", connection);
+            try
+            {
+                connection.Open();
+                aa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                aa.SelectCommand.Parameters.Add("@MAPYC", SqlDbType.VarChar, (10)).Value = txtMaPYC.Text;
+                //cmbMA.ValueMember = "MAMA";
+                cmbMA.Text=((MonAn)cmbMA.SelectedItem).MaMA;
+                aa.SelectCommand.Parameters.Add("@MAMA", SqlDbType.VarChar, (10)).Value = cmbMA.Text;
+                //aa.SelectCommand.Parameters.Add("@DONGIAMA", SqlDbType.Int).Value = txtLuong.Text;
+                aa.SelectCommand.Parameters.Add("@SOLUONGMA", SqlDbType.Int).Value = txtSoLuong.Text;
+                aa.SelectCommand.ExecuteNonQuery();
+                connection.Close();
+                //SqlDataReader dta = cmd.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
